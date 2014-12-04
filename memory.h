e4baadbac788eddba917fffb1ea1a78717f74989
Firas S. <choogiesaur@gmail.com>
2014-12-04 18:57:59 -0500

    Update memory.h
    doubly linked

--- memory.h
+++ memory.h
@@ -3,8 +3,12 @@
  */
  
 struct mem_block {
-	int size
+	int size;
+	int isFree;
+	struct mem_block *prev;
 	struct mem_block *next;
 	void *data;
 };
 typedef struct mem_block memBlock;
+
+int init_memBlock();
