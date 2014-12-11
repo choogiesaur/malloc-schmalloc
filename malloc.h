aa375519eb74d57baf9a256478f7c538f2c0fb77
obakir <wldkng14@yahoo.com>
2014-12-11 16:44:07 -0500

    Create malloc.h

---
+++ malloc.h
@@ -0,0 +1,13 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+struct MemEntry {
+    unsigned int size;
+    unsigned int free;
+    struct MemEntry *next, *prev;
+};
+
+void * my_malloc( unsigned int size, char * file, int line );
+void my_free( void * p, char * file, int line );
+
+#endif
