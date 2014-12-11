2b1e8b54dab9956363445cfcb034d30e4dfdc20b
obakir <wldkng14@yahoo.com>
2014-12-11 17:46:19 -0500

    Update malloc.h

--- malloc.h
+++ malloc.h
@@ -2,7 +2,8 @@
 #define MALLOC_H
 
 /*
- *
+ * Memory entry struct. Holds the size of the block, whether it is free or not, and pointers
+ * to the next and the previous memory entry structs.
  */
 struct MemEntry {
     unsigned int size;
@@ -11,12 +12,16 @@ struct MemEntry {
 };
 
 /*
- *
+ * Our malloc function. Takes an int containing the size of the chunk to be allocated, a character
+ * string for the filename, and an int for the line number. Returns a void pointer on success and
+ * NULL on failure.
  */
 void * my_malloc( unsigned int size, char * file, int line );
 
 /*
- *
+ * Our free function. Takes a void pointer to a memory chunk to be freed, a character string
+ * for the filename, and an int for the line number. Doesn't return anything but will print
+ * errors onto the screen if it encounters any.
  */
 void my_free( void * p, char * file, int line );
 
