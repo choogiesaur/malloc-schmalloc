c063ac26c6aee0a4a07271fb2bab0727b92b102a
obakir <wldkng14@yahoo.com>
2014-12-11 17:54:09 -0500

    Update malloc.c

--- malloc.c
+++ malloc.c
@@ -1,9 +1,9 @@
 #include	"malloc.h"
 #include    "sorted-list.h"
-#include	<unistd.h>
 #include	<stdio.h>
 #include	<string.h>
 #include	<errno.h>
+#include	<unistd.h>
 
 struct SortedListPtr sl = SLCreate();
 
