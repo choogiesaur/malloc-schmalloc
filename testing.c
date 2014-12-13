e88fa214800850e587bf5d9d3f1cae81f5827cf4
obakir <wldkng14@yahoo.com>
2014-12-13 18:40:11 -0500

    Update testing.c

--- testing.c
+++ testing.c
@@ -1,4 +1,6 @@
 #include "malloc.h"
+#include <stdlib.h>
+#include <stdio.h>
 #define malloc(x) myMalloc(x, __FILE__ , __LINE__)
 #define calloc(x) myCalloc(x, __FILE__, __LINE__)
 #define free(x) myFree(x, __FILE__, __LINE__)
@@ -9,7 +11,7 @@ void non_alloc_free() {
 	printf("Expected output: denied free\n\n");
 	int x;
 	free(x);
-	printf("\n***End non-allocated pointer free test***\n\n");
+	printf("\n***End non-allocated pointer free test***");
 	printf("\n---------------------------------------------------------------\n\n");
 
 }
@@ -18,9 +20,10 @@ void non_malloc_free() {
 	printf("\n---------------------------------------------------------------");
 	printf("\n***Start non-malloced pointer free test***\n\n");
 	printf("Expected output: denied free\n\n");
-	int ptr = (char *)malloc(500);
+	char *ptr = (char *)malloc(500);
+	//int ptr = (char *)malloc(500);
 	free(ptr + 50);
-	printf("\n***End non-malloced pointer free test***\n\n");
+	printf("\n***End non-malloced pointer free test***");
 	printf("\n---------------------------------------------------------------\n\n");
 }
 
@@ -31,7 +34,7 @@ void double_malloc_free() {
 	char *ptr = (char *)malloc(500);
 	free(ptr);
 	free(ptr);
-	printf("\n***End double malloc free test***\n\n");
+	printf("\n***End double malloc free test***");
 	printf("\n---------------------------------------------------------------\n\n");
 }
 
@@ -42,7 +45,7 @@ void double_calloc_free() {
 	char *ptr = (char *)calloc(500);
 	free(ptr);
 	free(ptr);
-	printf("\n***End double calloc free test***\n\n");
+	printf("\n***End double calloc free test***");
 	printf("\n---------------------------------------------------------------\n\n");
 }
 
@@ -54,7 +57,7 @@ void valid_malloc() {
 	free(ptr);
 	ptr = (char *)malloc(200); 
 	free(ptr);
-	printf("\n***End valid malloc test***\n\n");
+	printf("\n***End valid malloc test***");
 	printf("\n---------------------------------------------------------------\n\n");
 }
 
@@ -64,12 +67,12 @@ void valid_calloc() {
 	printf("Expected output: successful calloc, list of bytes (all are 0), successful free\n\n");
 	char *ptr = (char *)calloc(10);
 	int i;
-	for (i = 0; i < 10, i++) {
+	for (i = 0; i < 10; i++) {
 		printf("%d ", *(ptr + i));
 	}
 	printf("\n");
 	free(ptr);
-	printf("\n***End valid calloc test***\n\n");
+	printf("\n***End valid calloc test***");
 	printf("\n---------------------------------------------------------------\n\n");
 }
 
