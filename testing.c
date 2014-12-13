68d9df61d6a8b82532b5f975791f8cd2c6aae95e
obakir <wldkng14@yahoo.com>
2014-12-13 18:18:19 -0500

    Update testing.c

--- testing.c
+++ testing.c
@@ -1,77 +1,84 @@
 #include "malloc.h"
-#define malloc( x ) myMalloc( x, __FILE__ , __LINE__ )
-#define free( x ) myFree( x, __FILE__, __LINE__ )
+#define malloc(x) myMalloc(x, __FILE__ , __LINE__)
+#define calloc(x) myCalloc(x, __FILE__, __LINE__)
+#define free(x) myFree(x, __FILE__, __LINE__)
 
-void non_alloc_free() 
-{
-	printf("\n--------------------------------");
-	printf("\nBegin **freeing pointer not allocated** test. \n\n");
-
-	printf("You should expect to see a denied free \n\n");
-
+void non_alloc_free() {
+	printf("\n---------------------------------------------------------------");
+	printf("\n***Start non-allocated pointer free test***\n\n");
+	printf("Expected output: denied free\n\n");
 	int x;
-	free( x );
-
-	printf("\nEnd **freeing porinter not allocated** test.");
-	printf("\n------------------------------\n\n");
-
-}
-
-void non_malloc_free()
-{
-	printf("\n--------------------------------");
-	printf("\nBegin **freeing pointers not returned from malloc** test. \n\n");
-
-	printf("You should expect to see a denied free.\n\n");
-	
-	int p = (char *)malloc( 200 );
-	free( p + 10 );
-
-	printf("\nEnd **freeing pointers not returned from malloc** test.");
-	printf("\n------------------------------\n\n");
-
+	free(x);
+	printf("\n***End non-allocated pointer free test***\n\n");
+	printf("\n---------------------------------------------------------------\n\n");
 
 }
 
-void multi_free() 
-{	
-	printf("\n--------------------------------");
-	printf("\nBegin **redundant freeing** test. \n\n");
-
-	printf("You should expect to see an allocation, a free,");
-	printf("and then a denied free.\n\n");
-	
-	char * p= (char *)malloc( 200 );
-	free( p );
-	free( p );
-
-	printf("\nEnd **redundant freeing** test.");
-	printf("\n------------------------------\n\n");
+void non_malloc_free() {
+	printf("\n---------------------------------------------------------------");
+	printf("\n***Start non-malloced pointer free test***\n\n");
+	printf("Expected output: denied free\n\n");
+	int ptr = (char *)malloc(500);
+	free(ptr + 50);
+	printf("\n***End non-malloced pointer free test***\n\n");
+	printf("\n---------------------------------------------------------------\n\n");
 }
 
-void valid() 
-{
-	printf("\n--------------------------------");
-	printf("\nBegin **perfectly valid** test. \n\n");
-
-	printf("You should expect to see normal allocations.\n\n");	
-
-	char * p = (char *)malloc( 100 ); 
-	free( p );
-	p = (char *) malloc( 100 ); 
-	free( p );
-
-	printf("\nEnd **perfectly valid** test.");
-	printf("\n------------------------------\n\n");
-
-
+void double_malloc_free() {	
+	printf("\n---------------------------------------------------------------");
+	printf("\n***Start double malloc free test***\n\n");
+	printf("Expected output: successful malloc, successful free, denied free\n\n");
+	char *ptr = (char *)malloc(500);
+	free(ptr);
+	free(ptr);
+	printf("\n***End double malloc free test***\n\n");
+	printf("\n---------------------------------------------------------------\n\n");
 }
 
-int main(int argc, char const *argv[])
-{
+void double_calloc_free() {	
+	printf("\n---------------------------------------------------------------");
+	printf("\n***Start double calloc free test***\n\n");
+	printf("Expected output: successful calloc, successful free, denied free\n\n");
+	char *ptr = (char *)calloc(500);
+	free(ptr);
+	free(ptr);
+	printf("\n***End double calloc free test***\n\n");
+	printf("\n---------------------------------------------------------------\n\n");
+}
+
+void valid_malloc() {
+	printf("\n---------------------------------------------------------------");
+	printf("\n***Start valid malloc test***\n\n");
+	printf("Expected output: successful malloc, successful free, successful malloc, successful free\n\n");
+	char * ptr = (char *)malloc(200); 
+	free(ptr);
+	ptr = (char *)malloc(200); 
+	free(ptr);
+	printf("\n***End valid malloc test***\n\n");
+	printf("\n---------------------------------------------------------------\n\n");
+}
+
+void valid_calloc() {
+	printf("\n---------------------------------------------------------------");
+	printf("\n***Start valid calloc test***\n\n");
+	printf("Expected output: successful calloc, list of bytes (all are 0), successful free\n\n");
+	char *ptr = (char *)calloc(10);
+	int i;
+	for (i = 0; i < 10, i++) {
+		printf("%d ", *(ptr + i));
+	}
+	printf("\n");
+	free(ptr);
+	printf("\n***End valid calloc test***\n\n");
+	printf("\n---------------------------------------------------------------\n\n");
+}
+
+int main(int argc, char const *argv[]) {
 	non_alloc_free();
 	non_malloc_free();
-	multi_free();
-	valid();
+	double_malloc_free();
+	double_calloc_free();
+	valid_malloc();
+	valid_calloc();
 	return 0;
 }
