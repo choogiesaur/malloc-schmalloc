5960e24f3db53da46d359d3531843987132a9995
Firas S. <choogiesaur@gmail.com>
2014-12-13 19:25:57 -0500

    Update malloc.c

--- malloc.c
+++ malloc.c
@@ -8,51 +8,52 @@
 SortedListPtr sl;
 
 void * myMalloc(unsigned int size, char * fn, int ln) {
-	void 			*ret_ptr;
+	void 			*ret_ptr;	//Return pointer
 	
 	memEntry 		*ptr;
-	memEntry 		*successor;
+	memEntry 		*after;
 	
-	static memEntry 	*head = 0; //points to front of memEntry list
-	static memEntry 	*tail = 0; //points to back of memEntry list
+	static memEntry 	*head = 0;	//points to front of memEntry list
+	static memEntry 	*tail = 0;	//points to back of memEntry list
     
 	ptr = head;
 	while ( ptr != 0 )
 	{
-		if ( ptr->size < size ) {	//This block is too small.
+		if (ptr->size < size) {	//This block is too small.
 			ptr = ptr->next;
-		} else if ( !ptr->isFree ) {	//This block is not free.
+		} else if (ptr->isFree == 0) {	//This block is not free.
 			ptr = ptr->next;
-		} else if ( ptr->size < (size + sizeof(memEntry)) ) {	//This block is not big enough to cut up.
+		} else if (ptr->size < (size + sizeof(memEntry))) {	//This block is not big enough to cut up.
 			ptr->isFree = 0;
 			ret_ptr = (char *)ptr + sizeof(memEntry);
-			
+			// Adding this block to the list of blocks.
 			SLInsert(sl, ret_ptr);
+			printf("Alloc\'d block 0x%x\n", ret_ptr);
 			return ret_ptr;
-		} else {
-			successor = (memEntry *)((char *)ptr + sizeof(memEntry) + size); //offset from where ptr was; accounts for size of a mementry and the size of the block
-			successor->prev = ptr;
-			successor->next = ptr->next;
-			
+		} else { // Cretaing the zplit up block to come after our newly allocated block.
+			after = (memEntry *)((char *)ptr + sizeof(memEntry) + size); //offset from where ptr was; accounts for size of a mementry and the size of the block
+			after->prev = ptr;
+			after->next = ptr->next;
 			if(ptr->next != 0){
-				ptr->next->prev = successor;
+				ptr->next->prev = after;
 			}
-			
-			ptr->next = successor;
-			successor->size = ptr->size - sizeof(memEntry) - size;
-			successor->isFree = 1;
+			ptr->next = after;
+			after->size = ptr->size - sizeof(memEntry) - size;
+			after->isFree = 1;
 			ptr->size = size;
 			ptr->isFree = 0;
-			tail = (ptr == tail) ? successor : tail;
+			tail = (ptr == tail) ? after : tail;
 			ret_ptr = (char *)ptr + sizeof(memEntry);
-			
+			// Adding this block to the list of blocks.
 			SLInsert(sl, ret_ptr);
+			printf("Alloc\'d block 0x%x\n", ret_ptr);
 			return ret_ptr;
 		}
 	}
-	if ( (ptr = (memEntry *)sbrk( sizeof(memEntry) + size )) == (void *)-1 ) {
+	ptr = (memEntry *)sbrk(sizeof(memEntry) + size);
+	if (ptr == (void *)-1) {
 		return 0;
-	} else if ( tail == 0 ) { // tail is null, adds first one
+	} else if (tail == 0) { // tail is null, adds first one
 		ptr->prev = 0;
 		ptr->next = 0;
 		ptr->size = size;
@@ -60,8 +61,9 @@ void * myMalloc(unsigned int size, char * fn, int ln) {
 		head = tail = ptr;
 		ret_ptr = (char *)ptr + sizeof(memEntry);
 		sl = SLCreate(ptrcmp);
-		
+		// Adding this block to the list of blocks.
 		SLInsert(sl, ret_ptr);
+		printf("Alloc\'d block 0x%x\n", ret_ptr);
 		return ret_ptr;
 	} else	{ // otherwise add to existing list
 		ptr->prev = tail;
@@ -71,8 +73,9 @@ void * myMalloc(unsigned int size, char * fn, int ln) {
 		tail->next = ptr;
 		tail = ptr;
 		ret_ptr = (char *)ptr + sizeof(memEntry);
-		
+		// Adding this block to the list of blocks.
 		SLInsert(sl, ret_ptr);
+		printf("Alloc\'d block 0x%x\n", ret_ptr);
 		return ret_ptr;
 	}
 	printf("Error: Malloc failed in file %s at line %d\n", fn, ln);
@@ -86,57 +89,53 @@ void * myCalloc(unsigned int size, char * fn, int ln){
 		return 0;
 	}
 	memset((char *)ptr, 0, size);
+	printf("Calloc\'d block 0x%x\n", ptr);
 	return ptr;
 }
 
 void myFree(void * p, char * fn, int ln){
 	memEntry *ptr;
-	memEntry *before;
-	memEntry *after;
+	memEntry *before;	//memEntry for the previous block
+	memEntry *after;	//memEntry for the next block
     
 	if (sl == NULL) {
-		printf("No malloced/calloced memory in the list.\n");
+		printf("Error: No malloced/calloced memory in the list. Failed to free.\n");
 		return;
 	} if(SLFind(sl, p) == NULL) {
-		printf("This memory pointer was never malloced/calloced in the list.\n");
+		printf("Error: This memory pointer was never malloced/calloced in the list. Failed to free.\n");
 		return;
 	}
     
 	ptr = (memEntry *)((char *)p - sizeof(memEntry));
 
 	if ( (before = ptr->prev) != 0 && before->isFree ){
-		before->size += sizeof(memEntry) + ptr->size;	// Combining with before mementry
-		
+		before->size += sizeof(memEntry) + ptr->size;	// Merging with the previous memEntry free block
 		before->next  = 	ptr->next;
-		//adding
-		ptr->isFree = 		1;
+		ptr->isFree   = 		1;
 		before->next  = 	ptr->next;
 		if(ptr->next != 0){
 			ptr->next->prev = before;
 		}
-		//finish adding
 		SLRemove(sl, p);
-		//printf( "Freeing block %#x combining with beforeecessor new size is %d.\n", p, before->size );
-	}
-	else{   
+		//printf( "Freeing block %#x combining with predecessor new size is %d.\n", p, before->size );
+	} else{   
 		if (ptr->isFree == 0) {
-			//printf( "BKR freeing block %#x.\n", p );
-			SLRemove(sl, p);
+		    SLRemove(sl, p);
 		    ptr->isFree = 1;
 		    before = ptr;
-		} else printf("Error: Attempted to double free a pointer. Request denied. \n");
+		    printf("Freed block 0x%x\n", p);
+		} else {
+			printf("Error: Attempted to double free a pointer. Failed to free.\n");
+		}
 	}
 	if ( (after = ptr->next) != 0 && after->isFree ){
-		before->size += sizeof(memEntry) + after->size;	// merge with successor
+		before->size += sizeof(memEntry) + after->size;	// // Merging with the next memEntry free block
 		before->next = after->next;
-		//adding
 		before->isFree = 1;
-        
 		if(after->next != 0) {
 			after->next->prev = before;
 		}
-		//finish adding
-
+		
 		SLRemove(sl, p);
 		//printf( "freeing block %#x merging with successor new size is %d.\n", p, before->size );
 	}
